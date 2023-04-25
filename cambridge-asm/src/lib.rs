a1e9d66524e0a9fd55bdabc251899c4429eb0a79
SaadiSave <git.saadisave@gmail.com>
2023-04-26 00:39:29 +0200

    fix(clippy): set clippy to deny pedantic

--- cambridge-asm/src/lib.rs
+++ cambridge-asm/src/lib.rs
@@ -3,7 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#![warn(clippy::pedantic)]
+#![deny(clippy::pedantic)]
 #![allow(
     clippy::missing_errors_doc,
     clippy::missing_panics_doc,
