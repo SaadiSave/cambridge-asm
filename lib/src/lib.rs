55911b10ac00deef3340fab8f8dc3052fd09a900
SaadiSave <git.saadisave@gmail.com>
2023-05-05 21:45:07 +0200

    fix: deduplicate implementation of `TestStdout`

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -41,33 +41,9 @@ const PROGRAMS: [(&str, usize, &[u8]); 5] = [
 ];
 
 #[cfg(test)]
-use std::{io::Write, rc::Rc, sync::RwLock};
-
-#[cfg(test)]
-#[derive(Clone)]
-#[repr(transparent)]
-pub struct TestStdout(Rc<RwLock<Vec<u8>>>);
-
-#[cfg(test)]
-impl TestStdout {
-    pub fn new(s: impl std::ops::Deref<Target = [u8]>) -> Self {
-        Self(Rc::new(RwLock::new(s.to_vec())))
-    }
-
-    pub fn to_vec(self) -> Vec<u8> {
-        self.0.read().unwrap().clone()
-    }
+pub(crate) mod test_stdout {
+    include!("../test_stdout.rs");
 }
 
 #[cfg(test)]
-impl Write for TestStdout {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        self.0.write().unwrap().extend(buf);
-
-        Ok(buf.len())
-    }
-
-    fn flush(&mut self) -> std::io::Result<()> {
-        Ok(())
-    }
-}
+pub(crate) use test_stdout::TestStdout;
--- lib/tests/int_test.rs
+++ lib/tests/int_test.rs
@@ -1,33 +1,7 @@
 #[macro_use]
 extern crate cambridge_asm;
 
-use std::{io::Write, rc::Rc, sync::RwLock};
-
-#[derive(Clone)]
-#[repr(transparent)]
-pub struct TestStdout(Rc<RwLock<Vec<u8>>>);
-
-impl TestStdout {
-    pub fn new(s: impl std::ops::Deref<Target = [u8]>) -> Self {
-        Self(Rc::new(RwLock::new(s.to_vec())))
-    }
-
-    pub fn to_vec(self) -> Vec<u8> {
-        self.0.read().unwrap().clone()
-    }
-}
-
-impl Write for TestStdout {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        self.0.write().unwrap().extend(buf);
-
-        Ok(buf.len())
-    }
-
-    fn flush(&mut self) -> std::io::Result<()> {
-        Ok(())
-    }
-}
+include!("../test_stdout.rs");
 
 /// Extending the `Core` instruction set
 mod extension {
